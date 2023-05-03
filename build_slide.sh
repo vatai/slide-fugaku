f39f7f05a1cb51f2f8f9be63d89ae94716bd70ff
Tamás Ficsor <36206918+ficstamas@users.noreply.github.com>
2023-05-03 08:09:45 +0200

    Prepare dataset and config

--- build_slide.sh
+++ build_slide.sh
@@ -4,8 +4,8 @@ pushd HashingDeepLearning
 rm -rf bin
 mkdir bin
 pushd bin
-export CC="fcc -Nclang"
-export CXX="FCC -Nclang"
+export CC="fcc -Nclang -Kfast,openmp"
+export CXX="FCC -Nclang -Kfast,openmp"
 cmake ..
 cmake --build . -j
 ./runme ../SLIDE/Config_amz.csv
