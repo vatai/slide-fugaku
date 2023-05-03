3249b9dbe2a390a410b325d48ca8f00c6c906499
Emil VATAI <emil.vatai@gmail.com>
2023-05-03 12:01:58 +0900

    Update build

--- build_slide.sh
+++ build_slide.sh
@@ -1,10 +1,11 @@
 #!/usr/bin/bash
 
 pushd HashingDeepLearning
+rm -rf bin
 mkdir bin
 pushd bin
-cmake ..
-make -j CC=fcc CXX=FCC
+CC=fcc CXX=FCC cmake ..
+cmake --build . -j
 ./runme ../SLIDE/Config_amz.csv
 popd
 popd
